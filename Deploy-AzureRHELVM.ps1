d0d74d4fbb2b60bbcfd499bd5750d239276182e1
lorax79 <jordan.smith@powerhell.org>
2016-09-08 09:42:42 -0700

    Add files via upload

---
+++ Deploy-AzureRHELVM.ps1
@@ -0,0 +1,64 @@
+﻿
+param(
+[Parameter(Mandatory=$True)]
+$deploymentName,
+[Parameter(Mandatory=$True)]
+$ResourceGroupName,
+[Parameter(Mandatory=$True)]
+$vmNamePrefix,
+[Parameter(Mandatory=$True)]
+[int]$numberofVMinstances,
+[Parameter(Mandatory=$True)]
+[string]$RHELOSVersion,
+[Parameter(Mandatory=$True)]
+$virtualNetworkName,
+[Parameter(Mandatory=$True)]
+$subnetname,
+[Parameter(Mandatory=$True)]
+$vnetResourceGroupName,
+[Parameter(Mandatory=$True)]
+$storageAccountName,
+[Parameter(Mandatory=$True)]
+$subscriptionName,
+[Parameter(Mandatory=$True)]
+$AutomationAccountName,
+[Parameter(Mandatory=$True)]
+$OMSWorkspaceID,
+[Parameter(Mandatory=$True)]
+$OMSWorkspaceKey,
+[Parameter(Mandatory=$True)]
+$deploymentlocation
+)
+
+
+$templateuri = "https://raw.githubusercontent.com/lorax79/AzureTemplates/master/avm-base-RHEL.json"
+
+$apw = Get-AutomationVariable -Name "vmAdminPW" 
+
+
+$paramhash = @{
+              'adminUsername' = "LocalAdmin";
+              'vmNamePrefix' = $vmNamePrefix;
+              'workspaceid' = $OMSWorkspaceID;
+              'workspacekey' = $OMSWorkspaceKey;
+              'numberOfInstances' = $numberofVMinstances;
+              'adminPassword' = "$($apw)";
+              'rhelOSVersion' = $RHELOSVersion;
+              'subnetname' = $subnetname;
+              'virtualNetworkName' = $virtualNetworkName;
+              'virtualNetworkResourceGroup' = $vnetResourceGroupName;
+              'storageAccountName' = $storageAccountName
+               }
+               
+$cred = Get-AutomationPSCredential -Name $AutomationAccountName
+Login-AzureRmAccount -Credential $cred -SubscriptionName $subscriptionName -TenantId 
+
+$rg = Get-AzureRmResourceGroup -Name $ResourceGroupName -ea 0
+if (!($rg))
+    {
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $deploymentlocation
+    }
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name $deploymentName -Mode Incremental -TemplateFile $templateuri -TemplateParameterObject $paramhash
+
+ 

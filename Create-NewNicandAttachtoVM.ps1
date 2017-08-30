8e1f8c47047b5eccb8424163f08af1f78d0bfa53
lorax79 <jordan.smith@powerhell.org>
2017-08-30 16:25:22 -0700

    Added script for changing VM nic and adding to a new subnet

---
+++ Create-NewNicandAttachtoVM.ps1
@@ -0,0 +1,21 @@
+$vmName = ""
+$targetSubnetName = ""
+$targetVNETResourceGroupName = ""
+$VMresourceGroupName = ""
+$newNicName = ""
+$targetVnetName = ""
+$subscriptionName = ""
+
+
+#Login-AzureRmAccount
+
+Set-AzureRmContext -SubscriptionName $subscriptionName
+
+$vnet = Get-AzureRmVirtualNetwork -Name $targetVnetName -ResourceGroupName $targetVNETresourceGroupName
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $targetSubnetName
+$vm = get-azurermvm -Name $vmName -ResourceGroupName $VMResourceGroupName 
+$oldnicID = ($vm.NetworkProfile.NetworkInterfaces).id
+
+$newnic = New-AzureRmNetworkInterface -Name $newNicName -ResourceGroupName $VMResourceGroupName -Subnet $subnet -Location (Get-AzureRmResourceGroup -Name $VMresourceGroupName).location
+
+$vm | Remove-AzureRmVMNetworkInterface -NetworkInterfaceIDs $oldnicID | Add-AzureRmVMNetworkInterface -id $newnic.Id | Update-AzureRmVm
